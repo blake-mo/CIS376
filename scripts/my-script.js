23897cdf35cb40bcf734a937313fb189fbae6e7e
blake-mo <31947168+blake-mo@users.noreply.github.com>
2017-10-24 11:45:39 -0500

    Update my-script.js

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,13 +1,13 @@
 
 //I put everything in this function.
 
-//console.log('script file loaded...'); 
-//
-//$(document).ready(function() {
-//
-//	console.log('in my doc ready f/n');
-//
-//	$('.left-div').addClass('my-border'); //as soon as we load, give it a border
-//
-//
-//}); 
+console.log('script file loaded...'); 
+
+$(document).ready(function() {
+
+	console.log('in my doc ready f/n');
+
+	$('.left-div').addClass('my-border'); //as soon as we load, give it a border
+
+
+}); 
